e123767ef7acd979064a77135ff97140e36c9376
Mykola Kobets <mykola_kobets@epam.com>
2026-06-05 11:51:58 +0300

    demo_insurance: remove demo service because there is no vis resource
    Signed-off-by: Mykola Kobets <mykola_kobets@epam.com>

--- demo_insurance/src/telemetry_emulator/genivi/__init__.py
+++
@@ -1,5 +0,0 @@
-#
-#  Copyright (c) 2018-2021 Renesas Inc.
-#  Copyright (c) 2018-2021 EPAM Systems Inc.
-#
-
--- demo_insurance/src/telemetry_emulator/services/emulator_updaters/__init__.py
+++
@@ -1,7 +0,0 @@
-#
-#  Copyright (c) 2018-2021 Renesas Inc.
-#  Copyright (c) 2018-2021 EPAM Systems Inc.
-#
-
-from .vis_updater import VISUpdater
-from .rest_updater import RESTUpdater
